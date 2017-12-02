05e95093346e491df6cfdcac6a96d677ab9450b0
Andreas Taske <andreas@taske.de>
2017-12-02 23:31:57 +0000

    User can set degree of rotation with args

--- servocontrol/servocontrol.py
+++ servocontrol/servocontrol.py
@@ -1,30 +1,38 @@
+#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import sys, getopt
 
 servoPIN = 17
+min = 2.5
+max = 12.6
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
-p.start(2.5) # Initialisierung
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+p.start(2.5)
+  
+def main(argv):
+    if len(argv) == 1:
+        degree = int(argv[0])
+        if degree > 180 or degree < 0:
+            print "Degree must be between 0 and 180"
+            return
+            
+        print "degree", degree
+        range = max - min
+        pos = min + (range / 180 * degree)
+        print "pos", pos
+        p.ChangeDutyCycle(pos)
+        time.sleep(0.5)
+        p.stop()
+        GPIO.cleanup()
+        print "Rotate to " + argv[0] + "degree"
+    
+    else:
+        print "wrong paramter length"
+        
+    print argv
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
